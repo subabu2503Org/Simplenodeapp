d38ccda78f9b9a91105f32cb00da66ce7a249824
Suresh Warrier <48576944+subabu2503@users.noreply.github.com>
2024-12-06 03:32:28 +0000

    initial commit

---
+++ simplenodeapp/test.js
@@ -0,0 +1,14 @@
+## Add simple math test
+
+```bash
+
+# create a file named test.js with the following contents
+const assert = require('assert');
+describe('Simple Math Test', () => {
+ it('should return 2', () => {
+        assert.equal(1 + 1, 2);
+    });
+ it('should return 9', () => {
+        assert.equal(3 * 3, 9);
+    });
+});
